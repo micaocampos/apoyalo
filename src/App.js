ddb1593bc379e659c2371daefed7c8a1bd92276c
micaela ocampos <ocamposmicaela@gmail.com>
2024-03-02 23:50:16 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import PersonCard from './componentes/PersonCard';
 function App() {
   return (
     <div className="App">
-     <PersonCard firstName="Jane " lastName="Doe, " age="45" haircolor="Black" />
-     <PersonCard firstName="Jhon" lastName="Smith, " age="88" haircolor="Brown" />
-     <PersonCard firstName="Millard " lastName="Filmore, " age="50" haircolor="Brown" />
-     <PersonCard firstName="Maria " lastName="Smith, " age="62" haircolor="Brown" />
-    
-
+     <PersonCard firstName="Jane " lastName="Doe, " age={45} haircolor="Black" />
+     <PersonCard firstName="Jhon" lastName="Smith, " age={88} haircolor="Brown" />
+     <PersonCard firstName="Millard " lastName="Filmore, " age={50} haircolor="Brown" />
+     <PersonCard firstName="Maria " lastName="Smith, " age={62} haircolor="Brown" />
       </div>
         
   );
--- src/componentes/PersonCard.js
+++ src/componentes/PersonCard.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName:"",
+            lastName:"",
+            age:0,
+            haircolor:"",
+      };
+    }
     render() {
         const { firstName, lastName, age, haircolor } = this.props;
         return (
@@ -8,11 +17,21 @@ class PersonCard extends Component {
                <h3> {lastName} {firstName}</h3>
                 <p>age: {age}</p>
                 <p>Hair Color: {haircolor}</p>
-                
+                <button onClick={ this.sumaEdad()}>Brithday Button {firstName}</button>
             </div>
         );
 
     }
+    sumaEdad = (dato) => {
+        console.log(dato);
+        dato++;
+        this.age=dato;
+        console.log(dato);
+       
+    }
 }
+
+
+
 
 export default PersonCard;
